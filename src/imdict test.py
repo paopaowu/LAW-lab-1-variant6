14a3f2e1d7f788976eccdc78649f69728c6276a6
paopaowu <897873227@qq.com>
2021-04-10 16:52:16 +0800

    add property-based tests

--- src/imdict test.py
+++ src/imdict test.py
@@ -1,123 +1,173 @@
 import unittest
 import imdict
-
+from hypothesis import given
+import hypothesis.strategies as st
 
 
 class TestImmutableList(unittest.TestCase):
     def test_add(self):
-        root=imdict.node(1,1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        self.assertEqual(imdict.mytolist(root), [[0,2],[1,2],[2,2]])
+        root = imdict.node(1, 1)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        self.assertEqual(imdict.to_list(root), [[0, 2], [1, 2], [2, 2]])
 
     def test_remove(self):
-        root=imdict.node(1,1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        root=imdict.myremove(root,1)
-        self.assertEqual(imdict.mytolist(root), [[0,2],[2,2]])
+        root = imdict.node(1, 1)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        root = imdict.remove(root, 1)
+        self.assertEqual(imdict.to_list(root), [[0, 2], [2, 2]])
 
     def test_size(self):
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        self.assertEqual(imdict.mysize(root), 3)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        self.assertEqual(imdict.size(root), 3)
 
-    def test_Conversion(self):
-        root=imdict.myfromlist([[0,1],[2,1],[3,1]])
-        self.assertEqual(imdict.mytolist(root), [[0, 1], [2, 1], [3, 1]])
+    def test_to_list(self):
+        root = imdict.node(0, 1)
+        root = imdict.add(root, 2, 1)
+        root = imdict.add(root, 3, 1)
+        self.assertEqual(imdict.to_list(root), [[0, 1], [2, 1], [3, 1]])
+
+    def test_from_list(self):
+        root = imdict.from_list([[0, 1], [2, 1], [3, 1]])
+        self.assertEqual(imdict.to_list(root), [[0, 1], [2, 1], [3, 1]])
+
+    @given(st.lists(st.lists(st.integers(), min_size=2, max_size=2)))
+    def test_from_list_to_list_equality(self, a):
+        # The generated test data is processed
+        d = {}
+        for i in a:
+            d[i[0]] = i[1]
+        key_value = list(d.keys())
+        key_value.sort()
+        value_list = list(d.values())
+        c = []
+        for i in range(len(key_value)):
+            c.append([key_value[i], value_list[i]])
+
+        root = imdict.from_list(c)
+        b = imdict.to_list(root)
+        self.assertEqual(b, c)
+
+    @given(st.lists(st.lists(st.integers(), min_size=2, max_size=2)))
+    def test_monoid_identity(self, a):
+        # The generated test data is processed
+        d = {}
+        for i in a:
+            d[i[0]] = i[1]
+        key_value = list(d.keys())
+        key_value.sort()
+        value_list = list(d.values())
+        c = []
+        for i in range(len(key_value)):
+            c.append([key_value[i], value_list[i]])
+
+        self.assertEqual(imdict.to_list(imdict.mconcat(imdict.mempty(), imdict.from_list(c))), c)
+        self.assertEqual(imdict.to_list(imdict.mconcat(imdict.from_list(c), imdict.mempty())), c)
+
     def test_find(self):
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        self.assertEqual(imdict.myfind(root,2), 2)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        self.assertEqual(imdict.find(root, 2), 2)
 
-    def test_iterator(self) :
+    def test_iterator(self):
 
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        list=imdict.mytolist(root)
-        itor=iter(root)
-        test=[]
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        list = imdict.to_list(root)
+        itor = iter(root)
+        test = []
         while itor.has_next():
             test.append(next(itor))
         self.assertEqual(test, list)
 
-        self.assertRaises(StopIteration, lambda : next(itor))
+        self.assertRaises(StopIteration, lambda: next(itor))
+
     def test_filter(self):
         def func(k):
-            if k%2==0:
+            if k % 2 == 0:
                 return True
             return False
+
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        list=imdict.mytolist(root)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        list = imdict.to_list(root)
         list2 = []
         for i in range(len(list)):
             if func(list[i][0]):
                 list2.append(list[i])
 
-        itor=imdict.myfilter(root, func)
-        test=[]
+        itor = imdict.filter(root, func)
+        test = []
         while itor.has_next():
             test.append(next(itor))
         self.assertEqual(test, list2)
+
     def test_map(self):
         def func(k):
-            k+1
+            k + 1
+
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        list=imdict.mytolist(root)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        list = imdict.to_list(root)
         list2 = []
         for i in list:
             i[1] = func(i[1])
             list2.append(i)
 
-        itor=imdict.mymap(root, func)
-        test=[]
+        itor = imdict.map(root, func)
+        test = []
         while itor.has_next():
             test.append(next(itor))
         self.assertEqual(test, list2)
+
     def test_reduce(self):
-        def func(k,j):
-            return k+j
+        def func(k, j):
+            return k + j
+
         root = imdict.node(1, 1)
-        root =imdict.myadd(root, 1, 2)
-        root =imdict.myadd(root, 2, 2)
-        root =imdict.myadd(root, 0, 2)
-        sum=imdict.myreduce(iter(root), func)
+        root = imdict.add(root, 1, 2)
+        root = imdict.add(root, 2, 2)
+        root = imdict.add(root, 0, 2)
+        sum = imdict.reduce(iter(root), func)
         self.assertEqual(sum, 6)
+
     def test_dict(self):
-        d=imdict.dict()
+        d = imdict.dict()
         self.assertEqual(d.getting(1), None)
-        d.setting(0,1)
+        d.setting(0, 1)
         self.assertEqual(d.getting(0), 1)
         d.setting(0, 2)
         self.assertEqual(d.getting(0), 2)
         d.setting(1, None)
         self.assertEqual(d.getting(1), None)
-    def test_concat(self):
-        t1=imdict.node(1,1)
-        t1 =imdict.myadd(t1, 1, 2)
-        t1 =imdict.myadd(t1, 2, 2)
-        t1 =imdict.myadd(t1, 0, 2)
+
+    def test_mconcat(self):
+        t1 = imdict.node(1, 1)
+        t1 = imdict.add(t1, 1, 2)
+        t1 = imdict.add(t1, 2, 2)
+        t1 = imdict.add(t1, 0, 2)
         t2 = imdict.node(-1, 1)
-        t2 =imdict.myadd(t2, -1, 2)
-        t2 =imdict.myadd(t2, 3, 2)
-        t2 =imdict.myadd(t2, 1, 3)
-        t3=imdict.myconact(t1,t2)
-        self.assertEqual(imdict.mytolist(t3), [[-1,2],[0,2],[1,2],[2,2],[3,2]])
-        t3=imdict.myconact(t2,t1)
-        self.assertEqual(imdict.mytolist(t3), [[-1,2],[0,2],[1,2],[2,2],[3,2]])
+        t2 = imdict.add(t2, -1, 2)
+        t2 = imdict.add(t2, 3, 2)
+        t2 = imdict.add(t2, 1, 3)
+        t3 = imdict.mconcat(t1, t2)
+        self.assertEqual(imdict.to_list(t3), [[-1, 2], [0, 2], [1, 2], [2, 2], [3, 2]])
+        t3 = imdict.mconcat(t2, t1)
+        self.assertEqual(imdict.to_list(t3), [[-1, 2], [0, 2], [1, 2], [2, 2], [3, 2]])
+
 
 unittest.main()
--- src/imdict.py
+++ src/imdict.py
@@ -1,31 +1,32 @@
-
-
 class node:
+    def __init__(self, key, value, rightChild=None, leftChild=None):
+        self.k = key
+        self.v = value
+        self.rc = rightChild
+        self.lc = leftChild
+        self.count = 0
 
-    def __init__(self,key,value,rightChild=None,leftChild=None):
-        self.k=key
-        self.v=value
-        self.rc=rightChild
-        self.lc=leftChild
-        self.count=0
     def __iter__(self):
-        self.list=mytolist(self)
-        self.iter_count=-1
+        self.list = to_list(self)
+        self.iter_count = -1
         return self
+
     def __next__(self):
-        self.iter_count+=1
-        if len(self.list)<=self.iter_count:
+        self.iter_count += 1
+        if len(self.list) <= self.iter_count:
             raise StopIteration
         return self.list[self.iter_count]
+
     def has_next(self):
-        if len(self.list) <= self.iter_count+1:
+        if len(self.list) <= self.iter_count + 1:
             return False
         return True
 
 
-def myadd(tree,key, value):
-    copy_tree=mycopy(tree)
-    def func(tree,key,value):
+def add(tree, key, value):
+    copy_tree = copy(tree)
+
+    def func(tree, key, value):
         if key == tree.k:
             tree.v = value
             return True
@@ -41,126 +42,149 @@ def myadd(tree,key, value):
                 return True
             else:
                 return func(tree.rc, key, value)
-    func(copy_tree,key,value)
+
+    func(copy_tree, key, value)
     return copy_tree
-def mycopy(tree):
-    if tree==None:
+
+
+def copy(tree):
+    if tree == None:
         return None
-    copy_tree=node(tree.k,tree.v)
-    copy_tree.rc=mycopy(tree.rc)
-    copy_tree.lc = mycopy(tree.lc)
+    copy_tree = node(tree.k, tree.v)
+    copy_tree.rc = copy(tree.rc)
+    copy_tree.lc = copy(tree.lc)
     return copy_tree
-def mysize(tree):
+
+
+def size(tree):
     if tree != None:
-        tree.count=0
-        tree.count+=1
-        tree.count+=mysize(tree.rc)+mysize(tree.lc)
+        tree.count = 0
+        tree.count += 1
+        tree.count += size(tree.rc) + size(tree.lc)
         return tree.count
     else:
         return 0
 
-def myfromlist(list):
-    if len(list)==0:
+
+def from_list(list):
+    if len(list) == 0:
         return None
-    temp = list.pop()
+    temp = list[0]
     root = node(temp[0], temp[1])
-    while len(list)!=0:
-        temp=list.pop()
-        root=myadd(root,temp[0],temp[1])
+    i = 1
+    while i < len(list):
+        temp = list[i]
+        root = add(root, temp[0], temp[1])
+        i += 1
     return root
 
-def mytolist(tree):
-    list=[]
-    def  func(node,list):
-        if node!=None:
-            func(node.lc,list)
-            temp=[]
+
+def to_list(tree):
+    list = []
+
+    def func(node, list):
+        if node != None:
+            func(node.lc, list)
+            temp = []
             temp.append(node.k)
             temp.append(node.v)
             list.append(temp)
-            func(node.rc,list)
-    func(tree,list)
+            func(node.rc, list)
+
+    func(tree, list)
     return list
 
-def myfind(tree,key):
-    if tree.k==key:
+
+def find(tree, key):
+    if tree.k == key:
         return tree.v
     if key < tree.k:
         if tree.lc == None:
             return None
-        return myfind(tree.lc,key)
+        return find(tree.lc, key)
     if key > tree.k:
         if tree.lc == None:
             return None
-        return myfind(tree.rc, key)
+        return find(tree.rc, key)
 
-def myfilter(tree, func):
-    list=mytolist(tree)
-    list2=[]
+
+def filter(tree, func):
+    list = to_list(tree)
+    list2 = []
     for i in list:
         if func(i[0]):
-             list2.append(i)
-    root=myfromlist(list2)
+            list2.append(i)
+    root = from_list(list2)
     return iter(root)
 
-def mymap(tree, func):
-    list=mytolist(tree)
-    list2=[]
+
+def map(tree, func):
+    list = to_list(tree)
+    list2 = []
     for i in list:
-        i[1]=func(i[1])
+        i[1] = func(i[1])
         list2.append(i)
-    root=myfromlist(list2)
+    root = from_list(list2)
     return iter(root)
 
-def myreduce(treeitor,func):
+
+def reduce(treeitor, func):
     if treeitor.has_next():
-        res=next(treeitor)[1]
+        res = next(treeitor)[1]
     while treeitor.has_next():
-        res=func(res,next(treeitor)[1])
+        res = func(res, next(treeitor)[1])
     return res
 
-def myremove(tree, key):
-    list=mytolist(tree)
+
+def remove(tree, key):
+    list = to_list(tree)
     for i in range(len(list)):
-        if key==list[i][0]:
+        if key == list[i][0]:
             list.pop(i)
             break
-    return myfromlist(list)
+    return from_list(list)
 
-def myconact(t1,t2):
-    l1=mytolist(t1)
-    l2=mytolist(t2)
-    l3=[]
-    while (0<len(l1) and 0<len(l2)):
-        if l1[0][0]<l2[0][0]:
+
+def mconcat(t1, t2):
+    l1 = to_list(t1)
+    l2 = to_list(t2)
+    l3 = []
+    while (0 < len(l1) and 0 < len(l2)):
+        if l1[0][0] < l2[0][0]:
             l3.append(l1.pop(0))
-        elif l1[0][0]>l2[0][0]:
+        elif l1[0][0] > l2[0][0]:
             l3.append(l2.pop(0))
-        elif l1[0][0]==l2[0][0]:
-            if l1[0][1]>l2[0][1]:
+        elif l1[0][0] == l2[0][0]:
+            if l1[0][1] > l2[0][1]:
                 l3.append(l2.pop(0))
                 l1.pop(0)
             else:
                 l3.append(l1.pop(0))
                 l2.pop(0)
-    while len(l1)>0:
+    while len(l1) > 0:
         l3.append(l1.pop(0))
-    while len(l2)>0:
+    while len(l2) > 0:
         l3.append(l2.pop(0))
-    return myfromlist(l3)
+    return from_list(l3)
+
+
+def mempty():
+    return None
+
+
 class dict():
-    count=0
-    root=None
-    def getting(self,key):
-        if self.count==0:
+    count = 0
+    root = None
+
+    def getting(self, key):
+        if self.count == 0:
             return None
         else:
-            return myfind(self.root,key)
-    def setting(self,key,value):
-        if self.count==0:
-            self.root=node(key,value)
-            self.count+=1
+            return find(self.root, key)
+
+    def setting(self, key, value):
+        if self.count == 0:
+            self.root = node(key, value)
+            self.count += 1
         else:
-            self.root = myadd(self.root,key,value)
-
-
+            self.root = add(self.root, key, value)
